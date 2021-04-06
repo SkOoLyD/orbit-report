51e66584f5ba518b16433b0d8c9f18b9ed7c47f4
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-04-05 19:20:46 -0500

    part 2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+  sourceList : Satellite[];
+  displayList : Satellite[]; 
 }
